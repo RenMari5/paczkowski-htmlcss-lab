ee53dc8f6a18dae28bf0355cbc8fd219b49a62a7
Renee Paczkowski <rpaczkowski@gmail.com>
2022-04-21 21:50:09 -0400

    Linked JS code and played with console

---
+++ script.js
@@ -0,0 +1,31 @@
+// var name = "Ren";
+
+// name = "Bethany";
+
+// var name = "Kathy"
+
+// console.log(name);
+
+// let myName = "Johnny";
+
+// myName = "Bobby";
+
+// console.log(myName);
+
+// const anotherName = "Ash";
+
+// anotherName = "Jen";
+
+// console.log(anotherName);
+
+const me = {
+    name: "Ren",
+    age: 40,
+    height: "5 feet 3 inches",
+};
+
+console.log(me);
+
+me.age = 21; // changing information inside the object, so this works with const
+
+console.log(me);
